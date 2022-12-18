4221481f94c95e2f4f068d7f2b16299ebf5796c6
hosain-ghoraba <102357226+hosain-ghoraba@users.noreply.github.com>
2022-12-18 19:46:04 +0200

    ready to sumbit ....عااااااااااااا

--- Database Queries/Basic Structure.sql
+++ Database Queries/Basic Structure.sql
@@ -10,26 +10,6 @@ EXEC sp_MSForEachTable 'SET QUOTED_IDENTIFIER ON; DELETE FROM ?'
 EXEC sp_MSForEachTable 'ALTER TABLE ? WITH CHECK CHECK CONSTRAINT ALL'
 EXEC sp_MSForEachTable 'ENABLE TRIGGER ALL ON ?'
 
-go
-CREATE PROC dropAllTables
-AS
-	DECLARE @sql NVARCHAR(MAX);
-	SET @sql = N'';
-
-	SELECT @sql = @sql + N'
-	  ALTER TABLE ' + QUOTENAME(s.name) + N'.'
-	  + QUOTENAME(t.name) + N' DROP CONSTRAINT '
-	  + QUOTENAME(c.name) + ';'
-	FROM sys.objects AS c
-	INNER JOIN sys.tables AS t
-	ON c.parent_object_id = t.[object_id]
-	INNER JOIN sys.schemas AS s 
-	ON t.[schema_id] = s.[schema_id]
-	WHERE c.[type] IN ('D','C','F','PK','UQ')
-	ORDER BY c.[type];
-
-	EXEC sys.sp_executesql @sql;
-	EXEC sp_msforeachtable 'DROP TABLE ?';
 
 go
 create proc dropAllProceduresFunctionsViews
